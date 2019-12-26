796e81448ac6f1c92e2dfd1e5d022075b10d357b
Damnedlag <52156588+Damnedlag@users.noreply.github.com>
2019-12-26 22:39:59 +0100

    Add files via upload

---
+++ 05_book_titles/book.rb
@@ -0,0 +1,22 @@
+class Book 
+
+      def title=(str)
+        @title = str
+      end
+
+      
+
+def title
+    small_words = %w[on the and over of in a an] # %w(foo bar) is a shortcut for ["foo", "bar"]. Meaning it's a notation to write an array of strings separated by spaces instead of commas and without quotes around them. 
+    @title.split(' ').map.with_index do |w, i| # http://www.zenspider.com/ruby/quickref.html#types
+      unless (small_words.include? w) and (i > 0)
+        w.capitalize
+      else
+        w
+      end
+    end.join(' ')
+end
+
+end
+
+
